fbfb4bbb7cd8abbd67a24c70149763b5f8e90d29
GTsatiris <george.tsatiris@gmail.com>
2022-05-26 19:09:24 +0300

    Echoclient and server

---
+++ EchoClient/EchoClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoClient
+{
+    class EchoClient
+    {
+        static void Main(string[] args)
+        {
+            //create remote endpoint
+            //IPAddress remoteAddr = IPAddress.Parse("10.44.51.95");
+            IPAddress remoteAddr = IPAddress.Broadcast; //IPAddress.Broadcast is equiavalent to IPAddress.Parse("255.255.255.255")
+
+            IPEndPoint destinationEndPoint = new IPEndPoint(remoteAddr, 55555);
+
+            Console.WriteLine("Sending text data to " + destinationEndPoint.ToString());
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            
+            //YOU NEED THIS ONLY WHEN BROADCASTING
+            sock.EnableBroadcast = true;
+
+            while(true)
+            {
+                string text = Console.ReadLine();
+
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(text);
+
+                sock.SendTo(data, destinationEndPoint);
+            }
+        }
+    }
+}
---
+++ EchoServer/EchoServer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class EchoServer
+    {
+        static void Main(string[] args)
+        {
+            //SIMPLE NSLOOKUP LOOKALIKE
+            /*bool killProcess = false;
+            while (!killProcess)
+                killProcess = DnsLookup();*/
+
+            IPAddress addr = IPAddress.Any;//IPAddress.Any is equivalent to IPAddress.Parse("0.0.0.0")
+
+            IPEndPoint endPoint = new IPEndPoint(addr, 55555); //five fives :D
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            sock.Bind(endPoint);
+
+            Console.WriteLine("Opening socket, echoing from " + addr.ToString() + ":" + endPoint.Port);
+
+            byte[] data = new byte[1024];
+
+            while(true)
+            {
+                int numOfBytes = sock.Receive(data);
+                string text = System.Text.Encoding.ASCII.GetString(data, 0, numOfBytes);
+                Console.WriteLine(text);
+            }
+        }
+
+        //DNS service example
+        static bool DnsLookup()
+        {
+            Console.WriteLine("Please enter a host name...");
+            string hostname = Console.ReadLine();
+            if (hostname == "exit")
+                return true;
+            Console.WriteLine(hostname + " resolves to:");
+            IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+            foreach(IPAddress addr in addresses)
+            {
+                Console.WriteLine("- " + addr.ToString());
+            }
+            return false;
+        }
+    }
+}
