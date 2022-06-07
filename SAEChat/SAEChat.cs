c05c95e72d64789200810e8ea6a9d700d00803b6
GTsatiris <george.tsatiris@gmail.com>
2022-06-07 13:26:06 +0300

    SAEChat added

---
+++ SAEChat/SAEChat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SAEChat
+{
+    class SAEChat
+    {
+        static void Main(string[] args)
+        {
+
+            //OUTBOUND TRAFFIC INFRASTRUCTURE
+            IPAddress remoteAddr = IPAddress.Broadcast; //IPAddress.Broadcast is equiavalent to IPAddress.Parse("255.255.255.255")
+            IPEndPoint destinationEndPoint = new IPEndPoint(remoteAddr, 55555);
+
+            Console.WriteLine("Sending text data to " + destinationEndPoint.ToString());
+
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sock.EnableBroadcast = true;
+            //*******************************
+
+            //INBOUND TRAFFIC INFRASTRUCTURE
+            IPAddress addr = IPAddress.Any;//IPAddress.Any is equivalent to IPAddress.Parse("0.0.0.0")
+            IPEndPoint endPoint = new IPEndPoint(addr, 55555); //five fives :D
+
+            sock.Bind(endPoint);
+            Console.WriteLine("Opening socket, echoing from " + addr.ToString() + ":" + endPoint.Port);
+            //*******************************
+
+            byte[] data = new byte[1024];
+            while (true)
+            {
+                string text = Console.ReadLine();
+                data = System.Text.Encoding.ASCII.GetBytes(text);
+                sock.SendTo(data, destinationEndPoint);
+
+                int numOfBytes = sock.Receive(data);
+                text = System.Text.Encoding.ASCII.GetString(data, 0, numOfBytes);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
